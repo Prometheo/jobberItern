47a0965548ef3b85f98d9ce87b87be55b6f4b651
[prometheus] <[olaoyejnr@gmail.com]>
2020-01-14 23:59:04 +0100

    inputed data through shell

--- myscrumy/olaoyejnrscrumy/models.py
+++ myscrumy/olaoyejnrscrumy/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 class GoalStatus(models.Model):
     status_name = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.status_name
 
 class ScrumyGoals(models.Model):
     goal_name = models.CharField(max_length=200)
