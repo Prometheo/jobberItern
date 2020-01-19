fb4f160d4b86cae9536af617910e9c6c9307756a
[prometheus] <[olaoyejnr@gmail.com]>
2020-01-19 14:17:27 +0100

    did a couple of tinkling with the database through the shell

--- myscrumy/olaoyejnrscrumy/views.py
+++ myscrumy/olaoyejnrscrumy/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ScrumyGoals,ScrumyHistory,GoalStatus
+
 # Create your views here.
+new_message = ScrumyGoals.objects.filter(goal_name="Learn Django")[0].goal_name
 
 def get_grading_parameters(request):
-    return HttpResponse("This is a Scrum Application")
+    return HttpResponse(new_message)
