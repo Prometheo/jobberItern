acca6ba66aae4829309d42f6851b0211345f5e67
[prometheus] <[olaoyejnr@gmail.com]>
2020-02-08 12:21:24 +0100

    added send message view

--- myscrumy/websocket/views.py
+++ myscrumy/websocket/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import ConnectionModel, ChatMessage
 import json
+import boto3
 
 
 # Create your views here.
@@ -29,5 +30,21 @@ def disconnect(request):
     trash = body['connectionId']
     ConnectionModel.objects.get(connection_id = trash).delete()
     return JsonResponse({'message': 'disconnect successfully'}, status=200)
+
+def _send_to_connection(connection_id, data):
+    gatewayapi=boto3.client('apigatewaymanagementapi',endpoint_url=' https://085rlczqhe.execute-api.us-east-2.amazonaws.com/test/',region_name='us-east-2',aws_access_key_id='AKIAJE7DNVOVQJP7CBRQ',aws_secret_access_key='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')
+    return gatewayapi.post_to_connection(ConnectionId=connection_id,Data=json.dumps(data).encode('utf-8'))
+
+
+def send_message(request):
+    body = _parse_body(request.body)
+    ChatMessage.objects.create(message=body['message'],username=body['username'],timestamp=body['timestamp'])
+    connections = ConnectionModel.objects.all()
+    body = [body['message']]
+    data = {'message':body}
+    for cons in connections:
+        _send_to_connection(cons,data)
+    return JsonResponse({'message': 'successfully sent'}, status=200)
+
 
 
